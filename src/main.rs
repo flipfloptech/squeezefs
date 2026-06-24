b7cadee8951d89f6e59b8cd982366f7e9093bedb
Justin Oberdorf <109311040+flipfloptech@users.noreply.github.com>
2026-06-24 00:35:51 -0400

    change: make allow_other a standard boolean flag in CLI

--- src/main.rs
+++ src/main.rs
@@ -172,8 +172,8 @@ enum Commands {
         #[arg(long)]
         writeback: bool,
 
-        /// Allow other users to access the mount (default: false)
-        #[arg(long, default_value_t = false, action = clap::ArgAction::Set)]
+        /// Allow other users to access the mount
+        #[arg(long)]
         allow_other: bool,
 
         /// Validate backend storage connectivity on startup

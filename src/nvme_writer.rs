caaffde35b4c477f9303246813b753f9242f3813
Justin Oberdorf <109311040+flipfloptech@users.noreply.github.com>
2026-06-27 14:38:21 -0400

    feat(nvme_writer): implement write_block

--- src/nvme_writer.rs
+++ src/nvme_writer.rs
@@ -11,7 +11,29 @@ impl NvmeBlockWriter {
         }
     }
 
-    pub async fn write_block(&self, _offset: u64, _data: &[u8]) -> Result<()> {
-        unimplemented!("TDD: not yet implemented")
+    pub async fn write_block(&self, offset: u64, data: &[u8]) -> Result<()> {
+        use std::io::SeekFrom;
+        use tokio::fs::OpenOptions;
+        use tokio::io::{AsyncSeekExt, AsyncWriteExt};
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .open(&self.device_path)
+            .await
+            .map_err(|e| crate::error::SqueezefsError::Io(e))?;
+
+        file.seek(SeekFrom::Start(offset))
+            .await
+            .map_err(|e| crate::error::SqueezefsError::Io(e))?;
+
+        file.write_all(data)
+            .await
+            .map_err(|e| crate::error::SqueezefsError::Io(e))?;
+
+        file.sync_all()
+            .await
+            .map_err(|e| crate::error::SqueezefsError::Io(e))?;
+
+        Ok(())
     }
 }
